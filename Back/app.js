19950ce35da54a00f6bf1cac2ae5a5013e623b11
Lucas Lengrand <lucaslengranddev@gmail.com>
2024-07-21 19:12:15 +0200

    mise a jour hébergement API

--- Back/app.js
+++ Back/app.js
@@ -28,7 +28,7 @@ mongoose.connect(`${process.env.DB_LIEN}`, {
 
 // ajout des headers pour les requêtes
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://piquante.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
