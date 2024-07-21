15f184e4b54df77024a27a75dcbe90d9992bccb9
Lucas Lengrand <lucaslengranddev@gmail.com>
2024-07-21 19:00:56 +0200

    mise a jour hébergement API

--- Front/src/app/services/auth.service.ts
+++ Front/src/app/services/auth.service.ts
@@ -16,7 +16,7 @@ export class AuthService {
               private router: Router) {}
 
   createUser(email: string, password: string) {
-    return this.http.post<{ message: string }>('https://piquante-92332b6b8389.herokuapp.com/api/auth/signup', {email: email, password: password});
+    return this.http.post<{ message: string }>('https://piquante-3ifd.onrender.com/api/auth/signup', {email: email, password: password});
   }
 
   getToken() {
@@ -28,7 +28,7 @@ export class AuthService {
   }
 
   loginUser(email: string, password: string) {
-    return this.http.post<{ userId: string, token: string }>('https://piquante-92332b6b8389.herokuapp.com/api/auth/login', {email: email, password: password}).pipe(
+    return this.http.post<{ userId: string, token: string }>('https://piquante-3ifd.onrender.com/api/auth/login', {email: email, password: password}).pipe(
       tap(({ userId, token }) => {
         this.userId = userId;
         this.authToken = token;
